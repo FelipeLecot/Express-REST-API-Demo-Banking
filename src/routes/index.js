935709ff6a83c11231c7517319338bcdc2bac8b9
Felipe Lecot <47688222+FelipeLecot@users.noreply.github.com>
2023-02-21 18:13:02 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,15 +8,6 @@ let api = {
 }
 
 router.get('/:version(v[0-9]+)/:function', 
-	async (req, res, next) => {
-		let attemptAuth = await api.v1.auth(req);
-		if (attemptAuth.status == "ok") {
-			next();
-		}
-		else {
-			res.send({"status": "error", "errorCode": "403"});
-		}
-	},
 	async (req, res) => {
 		let requestedFunction = req.params["function"];
 		let functionVersion = req.params["version"];
@@ -33,25 +24,13 @@ router.get('/:version(v[0-9]+)/:function',
 	}
 );
 
-router.post('/auth/login', async (req, res) => {
-	try {
-		let apiFunction = api['v1']['login'];
-
-		res.send(await apiFunction(req));
-	}
-	catch (e){
-		res.send(e);
-		console.error(e);
-	}
-});
-
 router.get('/error/:error', async (req, res) => {
 	let errorCode = req.params["error"];
 
-	let allowedErrorCodes = ["403", "404", "500", "501", "503"];
+	let allowedErrorCodes = [403, 404, 500, 501, 503];
 
 	if (!allowedErrorCodes.includes(errorCode)) {
-		errorCode = "404";
+		errorCode = 404;
 	}
 	
 	res.status(200).send({status: "error", errorCode: errorCode});
